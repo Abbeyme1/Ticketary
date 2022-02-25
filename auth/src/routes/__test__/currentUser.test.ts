69117df88bbaba298fc820ae73d4acb2fb22edfe
Abhay <abhy1209120@gmail.com>
2022-02-26 00:40:46 +0530

    added test for currentUser

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -14,3 +14,13 @@ it('status 200 : get current user info',async () => {
 
     expect(res.body.currentUser.email).toEqual('test@test.com')
 })
+
+it('status 400 : unauthenticated user',async () => {
+    
+  const res = await request(app)
+  .get('/api/users/currentuser')
+  .send()
+  .expect(200)
+
+  expect(res.body.currentUser).toEqual(null)
+})
